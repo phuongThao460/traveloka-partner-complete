6c0131a7b53a2d351cba7e1bdf48b26c5111c23a
phuongThao460 <blackcompany1998@gmail.com>
2021-06-08 23:09:00 +0700

    change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./app/store";
+import { BrowserRouter } from 'react-router-dom';
+//import { Provider } from "react-redux";
+//import store from "./app/store";
 //import ListApartment from './components/pages/ListApartment'
 ReactDOM.render(
-  <Provider store={store}>
+  <BrowserRouter>
     <App />
-  </Provider>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
